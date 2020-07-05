1ec4ec6a002249c0e80e323a130f53041bddecc8
aarmono <codec2@aarmono.com>
2020-07-05 11:31:47 -0700

    Adding script to run alsamixer on each audio device or save settings to SD card

---
+++ configure.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+while [ $((`aplay -l | grep -c card`)) -lt 2 ]
+do
+    sleep .5
+done
+
+alsactl restore
+
+while [ true ]
+do
+    read -p 'Type "0" for headset, "1" for radio, or "s" to save and press Enter: ' option
+    case "$option" in
+        0)
+            alsamixer -c 0
+            ;;
+        1)
+            alsamixer -c 1
+            ;;
+        s)
+            alsactl store && mcopy -t -D o -i /dev/mmcblk0p1 /var/lib/alsa/asound.state ::config/asound.state
+            ;;
+        *)
+            "Invalid option"
+            ;;
+    esac
+done
+

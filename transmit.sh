a6ce10142b81d1702cefa6003387266bee99eb28
aarmono <codec2@aarmono.com>
2020-07-04 18:46:21 -0700

    Add missing file paths to commands

--- transmit.sh
+++ transmit.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-arecord -t raw -r 8000 -c 1 -f S16_LE -D "plughw:0,0" | crypto_tx /etc/crypto_tx.ini | aplay -t raw -r 48000 -c 1 -f S16_LE -D "plughw:1,0"
+arecord -t raw -r 8000 -c 1 -f S16_LE -D "plughw:0,0" - | crypto_tx /etc/crypto_tx.ini | aplay -t raw -r 48000 -c 1 -f S16_LE -D "plughw:1,0" -

2edb79da028bae374045c0e6f27abb6c9b516f87
aarmono <codec2@aarmono.com>
2020-07-04 23:36:34 -0700

    Wait until 2 audio devices are available before starting transmitter and receiver

--- transmit.sh
+++ transmit.sh
@@ -4,6 +4,11 @@ _term() {
     kill -TERM "$child"
 }
 
+while [ $((`aplay -l | grep -c card`)) -lt 2 ]
+do
+    sleep .5
+done
+
 trap _term SIGTERM
 
 arecord -t raw -r 8000 -c 1 -f S16_LE -D "plughw:0,0" -  | crypto_tx /etc/crypto_tx.ini | aplay -t raw -r 48000 -c 1 -f S16_LE -D "plughw:1,0" - &

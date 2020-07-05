f01bafcfa1c0ec879886d595ece10f4525d51520
aarmono <codec2@aarmono.com>
2020-07-04 19:29:22 -0700

    Add support for communicating SIGTERM to children

--- receive.sh
+++ receive.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env sh
 
-arecord -t raw -r 48000 -c 1 -f S16_LE -D "plughw:1,0" - | crypto_rx /etc/crypto_rx.ini | aplay -t raw -r 8000 -c 1 -f S16_LE -D "plughw:0,0" -
+_term() {
+    kill -TERM "$child"
+}
+
+trap _term SIGTERM
+
+arecord -t raw -r 48000 -c 1 -f S16_LE -D "plughw:1,0" - | crypto_rx /etc/crypto_rx.ini | aplay -t raw -r 8000 -c 1 -f S16_LE -D "plughw:0,0" - &
+child=$!
+wait "$child"
--- transmit.sh
+++ transmit.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env sh
 
-arecord -t raw -r 8000 -c 1 -f S16_LE -D "plughw:0,0" - | crypto_tx /etc/crypto_tx.ini | aplay -t raw -r 48000 -c 1 -f S16_LE -D "plughw:1,0" -
+_term() {
+    kill -TERM "$child"
+}
+
+trap _term SIGTERM
+
+arecord -t raw -r 8000 -c 1 -f S16_LE -D "plughw:0,0" -  | crypto_tx /etc/crypto_tx.ini | aplay -t raw -r 48000 -c 1 -f S16_LE -D "plughw:1,0" - &
+child=$!
+wait "$child"

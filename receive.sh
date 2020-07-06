c12c2ffdea74fff533836ec9ea4d01db4ee4e199
aarmono <codec2@aarmono.com>
2020-07-06 13:32:35 -0700

    Reduce latency of transmit and receive

--- receive.sh
+++ receive.sh
@@ -11,6 +11,8 @@ done
 
 trap _term SIGTERM
 
-arecord -t raw -r 48000 -c 1 -f S16_LE -D "plughw:1,0" - | crypto_rx /etc/crypto_rx.ini | aplay -t raw -r 8000 -c 1 -f S16_LE -D "plughw:0,0" - &
+# Record buffer is 40ms; Play buffer is 80ms. A frame length in crypto_tx and
+# crypto_rx is 40ms
+arecord -B 40000 -t raw -r 48000 -c 1 -f S16_LE -D "plughw:1,0" - | crypto_rx /etc/crypto_rx.ini | aplay -B 80000 -t raw -r 8000 -c 1 -f S16_LE -D "plughw:0,0" - &
 child=$!
 wait "$child"

0558f3367980472862440a4afb5e79a78a4ad631
aarmono <codec2@aarmono.com>
2020-07-19 18:33:38 -0700

    Move the starting sound out of crypto_tx to decrease lag

--- transmit.sh
+++ transmit.sh
@@ -15,4 +15,5 @@ trap _term SIGTERM
 # crypto_rx is 40ms
 arecord -B 40000 -t raw -r 8000 -c 1 -f S16_LE -D "plughw:0,0" -  | crypto_tx /etc/crypto_tx.ini | aplay -B 80000 -t raw -r 48000 -c 1 -f S16_LE -D "plughw:1,0" - &
 child=$!
+aplay -t wav -D "plug:headset" /usr/share/sounds/startup.wav
 wait "$child"

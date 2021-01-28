09a3c425000885d685b106f9021d6ed493e128a5
aarmono <codec2@aarmono.com>
2021-01-27 22:34:47 -0800

    Add sox bandpass filter to transmit.sh shell

--- transmit.sh
+++ transmit.sh
@@ -13,7 +13,7 @@ trap _term SIGTERM
 
 # Record buffer is 40ms; Play buffer is 80ms. A frame length in crypto_tx and
 # crypto_rx is 40ms
-arecord -B 40000 -t raw -r 8000 -c 1 -f S16_LE -D "plughw:0,0" -  | crypto_tx /etc/crypto_tx.ini | aplay -B 80000 -t raw -r 48000 -c 1 -f S16_LE -D "plughw:1,0" - &
+arecord -B 40000 -t raw -r 8000 -c 1 -f S16_LE -D "plughw:0,0" -  | sox -t raw -r 8000 -c 1 -b 16 -e signed-integer --buffer 320 - -t raw --buffer 320 - bandpass 1000 2000 | crypto_tx /etc/crypto_tx.ini | aplay -B 80000 -t raw -r 48000 -c 1 -f S16_LE -D "plughw:1,0" - &
 child=$!
 aplay -t wav -D "plug:headset" /usr/share/sounds/startup.wav
 wait "$child"

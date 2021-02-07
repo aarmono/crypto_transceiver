da94e0979a77494696d1339d4a0f071662bfd051
aarmono <codec2@aarmono.com>
2021-02-06 22:29:04 -0800

    Restart transmit and receive services if they prematurely terminate

--- receive.sh
+++ receive.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env sh
 
+RESTART=1
+
 _term() {
     kill -TERM "$child"
+    RESTART=0
 }
 
-while [ $((`aplay -l | grep -c card`)) -lt 2 ]
+while [ $RESTART -ne 0 ]
 do
-    sleep .5
+
+    while [ $((`aplay -l | grep -c card`)) -lt 2 ]
+    do
+        sleep .5
+    done
+
+    trap _term SIGTERM
+
+    # Record buffer is 40ms; Play buffer is 80ms. A frame length in crypto_tx and
+    # crypto_rx is 40ms
+    arecord -B 40000 -t raw -r 48000 -c 1 -f S16_LE -D "plughw:1,0" - | crypto_rx /etc/crypto_rx.ini | aplay -B 80000 -t raw -r 8000 -c 1 -f S16_LE -D "plug:headset" - &
+    child=$!
+    wait "$child"
+
 done
-
-trap _term SIGTERM
-
-# Record buffer is 40ms; Play buffer is 80ms. A frame length in crypto_tx and
-# crypto_rx is 40ms
-arecord -B 40000 -t raw -r 48000 -c 1 -f S16_LE -D "plughw:1,0" - | crypto_rx /etc/crypto_rx.ini | aplay -B 80000 -t raw -r 8000 -c 1 -f S16_LE -D "plug:headset" - &
-child=$!
-wait "$child"
--- transmit.sh
+++ transmit.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env sh
 
+RESTART=1
+
 _term() {
     kill -TERM "$child"
+    RESTART=0
 }
 
-while [ $((`aplay -l | grep -c card`)) -lt 2 ]
+while [ $RESTART -ne 0 ]
 do
-    sleep .5
+
+    while [ $((`aplay -l | grep -c card`)) -lt 2 ]
+    do
+        sleep .5
+    done
+
+    trap _term SIGTERM
+
+    # Record buffer is 40ms; Play buffer is 80ms. A frame length in crypto_tx and
+    # crypto_rx is 40ms
+    record_voice.sh | crypto_tx /etc/crypto_tx.ini | aplay -B 160000 -t raw -r 48000 -c 1 -f S16_LE -D "plughw:1,0" - &
+    child=$!
+    aplay -t wav -D "plug:headset" /usr/share/sounds/startup.wav
+    wait "$child"
+
 done
-
-trap _term SIGTERM
-
-# Record buffer is 40ms; Play buffer is 80ms. A frame length in crypto_tx and
-# crypto_rx is 40ms
-record_voice.sh | crypto_tx /etc/crypto_tx.ini | aplay -B 160000 -t raw -r 48000 -c 1 -f S16_LE -D "plughw:1,0" - &
-child=$!
-aplay -t wav -D "plug:headset" /usr/share/sounds/startup.wav
-wait "$child"

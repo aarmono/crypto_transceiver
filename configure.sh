73b0c084bcaf9964f3fef2a5150333564ba273b1
aarmono <codec2@aarmono.com>
2021-02-10 22:22:27 -0800

    Add options for enabling/disabling encryption using the keyboard

--- configure.sh
+++ configure.sh
@@ -25,6 +25,16 @@ do
                 aplay -q -t wav -D "plug:headset" /usr/share/sounds/nack.wav
             fi
             ;;
+        p)
+            sed 's/^[[:blank:]]*KeyFile/;KeyFile/' /etc/crypto_tx.ini > /etc/crypto_tx.ini.new && mv /etc/crypto_tx.ini.new /etc/crypto_tx.ini
+            sed 's/^[[:blank:]]*KeyFile/;KeyFile/' /etc/crypto_rx.ini > /etc/crypto_rx.ini.new && mv /etc/crypto_rx.ini.new /etc/crypto_rx.ini
+            killall -SIGHUP crypto_tx && killall -SIGHUP crypto_rx
+            ;;
+        c)
+            sed 's/^[[:blank:]]*;[[:blank:]]*KeyFile/KeyFile/' /etc/crypto_tx.ini > /etc/crypto_tx.ini.new && mv /etc/crypto_tx.ini.new /etc/crypto_tx.ini
+            sed 's/^[[:blank:]]*;[[:blank:]]*KeyFile/KeyFile/' /etc/crypto_rx.ini > /etc/crypto_rx.ini.new && mv /etc/crypto_rx.ini.new /etc/crypto_rx.ini
+            killall -SIGHUP crypto_tx && killall -SIGHUP crypto_rx
+            ;;
         l)
             /sbin/getty -L tty1 115200 vt100
             ;;

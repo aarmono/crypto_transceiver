56ff3d9a511296110f5dacb3343459833d1181e3
aarmono <crypto@aarmono.com>
2023-06-19 14:23:39 -0700

    Increase modem transmit buffer size

--- transmit.sh
+++ transmit.sh
@@ -26,7 +26,7 @@ do
     # Record buffer is 40ms; Play buffer is 80ms. A frame length in crypto_tx and
     # crypto_rx is 40ms
     SAMPLE_RATE=`crypto_sample_rate /etc/crypto_tx.ini`
-    record_voice.sh | crypto_tx /etc/crypto_tx.ini | aplay -B 160000 -t raw -r $SAMPLE_RATE -c 1 -f S16_LE -D "plughw:1,0" - &
+    record_voice.sh | crypto_tx /etc/crypto_tx.ini | aplay -B 320000 -t raw -r $SAMPLE_RATE -c 1 -f S16_LE -D "plughw:1,0" - &
     child=$!
     aplay -t wav -D "plug:headset" /usr/share/sounds/startup.wav
     wait "$child"

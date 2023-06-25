603231fbc68b2016d3698525a91857b296cac9c6
aarmono <crypto@aarmono.com>
2023-06-24 21:28:58 -0700

    Updating scripts to work with new crypto.ini file

--- configure.sh
+++ configure.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env sh
 
+disable_encryption ()
+{
+    sed 's/^[[:blank:]]*KeyFile/;KeyFile/' /etc/crypto.ini > /etc/crypto.ini.new && mv /etc/crypto.ini.new /etc/crypto.ini
+}   killall -SIGHUP jack_crypto_tx jack_crypto_rx
+
+enable_encryption ()
+{
+    sed 's/^[[:blank:]]*;[[:blank:]]*KeyFile/KeyFile/' /etc/crypto.ini > /etc/crypto.ini.new && mv /etc/crypto.ini.new /etc/crypto.ini
+    killall -SIGHUP jack_crypto_tx jack_crypto_rx
+}
+
 while [ $((`aplay -l | grep -c card`)) -lt 2 ]
 do
     sleep .5
@@ -18,22 +29,12 @@ do
             ;;
         s)
             alsactl store && mcopy -t -D o -i /dev/mmcblk0p1 /var/lib/alsa/asound.state ::config/asound.state
-            if [ $? = 0 ]
-            then
-                aplay -q -t wav -D "plug:headset" /usr/share/sounds/ack.wav
-            else
-                aplay -q -t wav -D "plug:headset" /usr/share/sounds/nack.wav
-            fi
             ;;
         p)
-            sed 's/^[[:blank:]]*KeyFile/;KeyFile/' /etc/crypto_tx.ini > /etc/crypto_tx.ini.new && mv /etc/crypto_tx.ini.new /etc/crypto_tx.ini
-            sed 's/^[[:blank:]]*KeyFile/;KeyFile/' /etc/crypto_rx.ini > /etc/crypto_rx.ini.new && mv /etc/crypto_rx.ini.new /etc/crypto_rx.ini
-            killall -SIGHUP crypto_tx && killall -SIGHUP crypto_rx
+            disable_encryption
             ;;
         c)
-            sed 's/^[[:blank:]]*;[[:blank:]]*KeyFile/KeyFile/' /etc/crypto_tx.ini > /etc/crypto_tx.ini.new && mv /etc/crypto_tx.ini.new /etc/crypto_tx.ini
-            sed 's/^[[:blank:]]*;[[:blank:]]*KeyFile/KeyFile/' /etc/crypto_rx.ini > /etc/crypto_rx.ini.new && mv /etc/crypto_rx.ini.new /etc/crypto_rx.ini
-            killall -SIGHUP crypto_tx && killall -SIGHUP crypto_rx
+            enable_encryption
             ;;
         l)
             /sbin/getty -L tty1 115200 vt100

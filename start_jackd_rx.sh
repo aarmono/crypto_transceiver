429767913259951c5e8528aec9c2ddc63a7bbb18
aarmono <crypto@aarmono.com>
2023-06-25 21:16:43 -0700

    Transmit and Receive specific buffer configuration

--- start_jackd_rx.sh
+++ start_jackd_rx.sh
@@ -16,6 +16,6 @@ else
 fi
 
 SAMPLE_RATE=`iniget JACK SampleRateRX /etc/crypto.ini.sd /etc/crypto.ini`
-BUFFERS=`iniget JACK NumBuffers /etc/crypto.ini.sd /etc/crypto.ini`
+BUFFERS=`iniget JACK NumBuffersRX /etc/crypto.ini.sd /etc/crypto.ini`
 
 exec jackd -n rx -d alsa $HW_ARGS -r "$SAMPLE_RATE" -p 1024 -n "$BUFFERS"

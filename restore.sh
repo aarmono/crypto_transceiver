2c98bd1bdff0069c1af13d7b470d5856598494e6
aarmono <crypto@aarmono.com>
2023-07-09 18:21:16 -0700

    Get random seed from /dev/random and do so in a background process so it doesn't block startup

--- restore.sh
+++ restore.sh
@@ -6,3 +6,9 @@ do
 done
 
 alsactl restore
+
+# Seed the RNG with random data from the SD card, if available
+mcopy -D o -n -i /dev/mmcblk0p1 ::seed /var/run/random-seed && dd if=/var/run/random-seed of=/dev/urandom
+# Put a new seed onto the SD card. This call will block until the RNG is
+# initialized, so this needs to run in a background process (it is)
+dd if=/dev/random of=/var/run/random-seed bs=512 count=1 && mcopy -D o -n -i /dev/mmcblk0p1 /var/run/random-seed ::seed

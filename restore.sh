cc4d967f70ff054296eba4fa0a41f99a1fbb6c6c
aarmono <crypto@aarmono.com>
2023-07-12 18:08:49 -0700

    Add logic to upgrade the in-memory asound.state file using the new static device names

--- restore.sh
+++ restore.sh
@@ -1,18 +1,31 @@
 #!/usr/bin/env sh
 
-# Seed the RNG with random data from the SD card, if available
-# Do this before the wait loop so it's done as early in the boot
-# process as possible
-mcopy -D o -n -i /dev/mmcblk0p1 ::seed /var/run/random-seed && dd if=/var/run/random-seed of=/dev/urandom
+ASOUND=/var/lib/alsa/asound.state
+ASOUND_NEW="$ASOUND".new
 
-while [ $((`aplay -l | grep -c card`)) -lt 2 ]
-do
-    sleep .5
-done
+# Don't modify the file if we don't need to
+if ! grep -q "state.$1 " "$ASOUND"
+then
+    # Make sure ALSA sees this card
+    while [ $((`aplay -l | grep -c "$1"`)) -lt 1 ]
+    do
+        sleep .5
+    done
+
+    # Find the hardware number
+    DEVNUM=`aplay -l | grep "$1" | grep -o -E "card \w" | cut -d ' ' -f 2`
+
+    # In legacy asound.state files, card 0 had a device name
+    # of "Device" and card 1 had a device name of "Device_1"
+    if [ "$DEVNUM" = "0" ]
+    then
+        DEVNAME="Device"
+    else
+        DEVNAME="Device_1"
+    fi
+
+    # Rewrite the asound.state with the new device name
+    sed -e "s/state.$DEVNAME /state.$1 /g" < "$ASOUND" > "$ASOUND_NEW" && mv "$ASOUND_NEW" "$ASOUND"
+fi
 
 alsactl restore
-
-# Put a new seed onto the SD card. This call will block until the RNG is
-# initialized, so this needs to run in a background process (it is)
-# Do this at the end so the alsactl call isn't blocked by it
-dd if=/dev/random of=/var/run/random-seed bs=512 count=1 && mcopy -D o -n -i /dev/mmcblk0p1 /var/run/random-seed ::seed

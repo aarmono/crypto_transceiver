809957ba4ff62de3c74d20b7434c09ec223f70d3
aarmono <crypto@aarmono.com>
2023-07-25 23:25:09 -0700

    Rename Combo section to Selector

--- key_combo.sh
+++ key_combo.sh
@@ -5,11 +5,11 @@ trap "kill %1; exit 0" INT TERM
 
 wait_initialized
 
-A_PIN=`get_config_val Combo AGPIONum`
-B_PIN=`get_config_val Combo BGPIONum`
-D_PIN=`get_config_val Combo DGPIONum`
-BIAS=`get_config_val Combo Bias`
-ACTIVE=`get_config_val Combo ActiveLow`
-DEBOUNCE=`get_config_val Combo Debounce`
+A_PIN=`get_config_val Selector AGPIONum`
+B_PIN=`get_config_val Selector BGPIONum`
+D_PIN=`get_config_val Selector DGPIONum`
+BIAS=`get_config_val Selector Bias`
+ACTIVE=`get_config_val Selector ActiveLow`
+DEBOUNCE=`get_config_val Selector Debounce`
 
 exec key_combo "$A_PIN" "$B_PIN" "$D_PIN" "$BIAS" "$ACTIVE" "$DEBOUNCE"
